253c0a5ed48cbbd08569d392221272cdba6bb257
Eugen Mayer <eugen.mayer@kontextwork.de>
2017-11-09 16:14:15 +0100

    upgraded entrypoint script from
    https://github.com/blacklabelops/confluence/edit/master/docker-entrypoint.sh

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 #
 # A helper script for ENTRYPOINT.
 #
@@ -45,7 +45,7 @@ function processConfluenceProxySettings() {
 
 function processContextPath() {
   if [ -n "${CONFLUENCE_CONTEXT_PATH}" ]; then
-    xmlstarlet ed -P -S -L --update "//Context/@path" --value "${CONFLUENCE_CONTEXT_PATH}" ${CONF_INSTALL}/conf/server.xml
+    xmlstarlet ed -P -S -L --update "//Context[contains(@docBase,'../confluence')]/@path" --value "${CONFLUENCE_CONTEXT_PATH}" ${CONF_INSTALL}/conf/server.xml
   fi
 }
 
@@ -64,6 +64,98 @@ function relayConfluenceLogFiles() {
   echo "${TARGET_PROPERTY} = ${confluence_logfile}" >> ${CONF_INSTALL}/conf/logging.properties
 }
 
+function setConfluenceConfigurationProperty() {
+  local configurationProperty=$1
+  local configurationValue=$2
+  if [ -n "${configurationProperty}" ]; then
+    local propertyCount=$(xmlstarlet sel -t -v "count(//property[@name='${configurationProperty}'])" ${CONF_HOME}/confluence.cfg.xml)
+    if [ "${propertyCount}" = '0' ]; then
+      # Element does not exist, we insert new property
+      xmlstarlet ed --pf --inplace --subnode '//properties' --type elem --name 'property' --value "${configurationValue}" -i '//properties/property[not(@name)]' --type attr --name 'name' --value "${configurationProperty}" ${CONF_HOME}/confluence.cfg.xml
+    else
+      # Element exists, we update the existing property
+      xmlstarlet ed --pf --inplace --update "//property[@name='${configurationProperty}']" --value "${configurationValue}" ${CONF_HOME}/confluence.cfg.xml
+    fi
+  fi
+}
+
+function processConfluenceConfigurationSettings() {
+  local counter=1
+  if [ -f "${CONF_HOME}/confluence.cfg.xml" ]; then
+    for (( counter=1; ; counter++ ))
+    do
+      VAR_CONFLUENCE_CONFIG_PROPERTY="CONFLUENCE_CONFIG_PROPERTY$counter"
+      VAR_CONFLUENCE_CONFIG_VALUE="CONFLUENCE_CONFIG_VALUE$counter"
+      if [ -z "${!VAR_CONFLUENCE_CONFIG_PROPERTY}" ]; then
+        break
+      fi
+      setConfluenceConfigurationProperty ${!VAR_CONFLUENCE_CONFIG_PROPERTY} ${!VAR_CONFLUENCE_CONFIG_VALUE}
+    done
+  fi
+}
+
+function processCatalinaDefaultConfiguration() {
+  if [ -f "${CONF_INSTALL}/bin/setenv.sh" ]; then
+    sed -i "/export CATALINA_OPTS/d" ${CONF_INSTALL}/bin/setenv.sh
+    sed -i "/CATALINA_OPTS=/d" ${CONF_INSTALL}/bin/setenv.sh
+    echo 'CATALINA_OPTS="-Dconfluence.document.conversion.fontpath=/usr/share/fonts/truetype/msttcorefonts ${CATALINA_OPTS}"
+CATALINA_OPTS="-XX:-PrintGCDetails ${CATALINA_OPTS}"
+CATALINA_OPTS="-XX:+PrintGCDateStamps ${CATALINA_OPTS}"
+CATALINA_OPTS="-XX:-PrintTenuringDistribution ${CATALINA_OPTS}"
+CATALINA_OPTS="-Xloggc:$LOGBASEABS/logs/gc-`date +%F_%H-%M-%S`.log ${CATALINA_OPTS}"
+CATALINA_OPTS="-XX:+UseGCLogFileRotation ${CATALINA_OPTS}"
+CATALINA_OPTS="-XX:NumberOfGCLogFiles=5 ${CATALINA_OPTS}"
+CATALINA_OPTS="-XX:GCLogFileSize=2M ${CATALINA_OPTS}"
+CATALINA_OPTS="-XX:G1ReservePercent=20 ${CATALINA_OPTS}"
+CATALINA_OPTS="-Djava.awt.headless=true ${CATALINA_OPTS}"
+CATALINA_OPTS="-Datlassian.plugins.enable.wait=300 ${CATALINA_OPTS}"
+CATALINA_OPTS="-Xms1024m ${CATALINA_OPTS}"
+CATALINA_OPTS="-Xmx1024m ${CATALINA_OPTS}"
+CATALINA_OPTS="-XX:+UseG1GC ${CATALINA_OPTS}"
+CATALINA_OPTS="${START_CONFLUENCE_JAVA_OPTS} ${CATALINA_OPTS}"
+CATALINA_OPTS="-Dsynchrony.enable.xhr.fallback=true ${CATALINA_OPTS}"
+CATALINA_OPTS="-Dorg.apache.tomcat.websocket.DEFAULT_BUFFER_SIZE=32768 ${CATALINA_OPTS}"
+CATALINA_OPTS="-Dconfluence.context.path=${CONFLUENCE_CONTEXT_PATH} ${CATALINA_OPTS}"' >> ${CONF_INSTALL}/bin/setenv.sh
+    echo "export CATALINA_OPTS" >> ${CONF_INSTALL}/bin/setenv.sh
+  fi
+}
+
+function setCatalinaConfigurationProperty() {
+  local configurationProperty=$1
+  local configurationValue=$2
+  local catalinaproperty=""
+  if [ -n "${configurationProperty}" ]; then
+    sed -i "/${configurationProperty}/d" ${CONF_INSTALL}/bin/setenv.sh
+    catalinaproperty="CATALINA_OPTS=\""${configurationProperty}
+    if [ -n "${configurationValue}" ]; then
+      catalinaproperty=${catalinaproperty}${configurationValue}
+    fi
+    catalinaproperty=${catalinaproperty}" "'${CATALINA_OPTS}'"\""
+    echo ${catalinaproperty} >> ${CONF_INSTALL}/bin/setenv.sh
+  fi
+}
+
+function processCatalinaConfigurationSettings() {
+  local counter=1
+  local VAR_CATALINA_PARAMETER="CATALINA_PARAMETER1"
+  local VAR_CATALINA_PARAMETER_VALUE="CATALINA_PARAMETER_VALUE1"
+  if [ -n "${!VAR_CATALINA_PARAMETER}" ]; then
+    if [ -f "${CONF_INSTALL}/bin/setenv.sh" ]; then
+      sed -i "/export CATALINA_OPTS/d" ${CONF_INSTALL}/bin/setenv.sh
+      for (( counter=1; ; counter++ ))
+      do
+        VAR_CATALINA_PARAMETER="CATALINA_PARAMETER$counter"
+        VAR_CATALINA_PARAMETER_VALUE="CATALINA_PARAMETER_VALUE$counter"
+        if [ -z "${!VAR_CATALINA_PARAMETER}" ]; then
+          break
+        fi
+        setCatalinaConfigurationProperty ${!VAR_CATALINA_PARAMETER} ${!VAR_CATALINA_PARAMETER_VALUE}
+      done
+      echo "export CATALINA_OPTS" >> ${CONF_INSTALL}/bin/setenv.sh
+    fi
+  fi
+}
+
 if [ -n "${CONFLUENCE_DELAYED_START}" ]; then
   sleep ${CONFLUENCE_DELAYED_START}
 fi
@@ -74,6 +166,16 @@ processConfluenceProxySettings
 
 processContextPath
 
+processConfluenceConfigurationSettings
+
+if [ -n "${CATALINA_PARAMETER1}" ]; then
+  processCatalinaDefaultConfiguration
+fi
+
+if [ -n "${CATALINA_PARAMETER1}" ]; then
+  processCatalinaConfigurationSettings
+fi
+
 if [ -n "${CONFLUENCE_LOGFILE_LOCATION}" ]; then
   processConfluenceLogfileSettings
   relayConfluenceLogFiles
@@ -82,7 +184,8 @@ fi
 /usr/local/bin/custom_scripts.sh
 
 if [ "$1" = 'confluence' ]; then
-  exec /opt/atlassian/confluence/bin/start-confluence.sh -fg
+  source /usr/bin/dockerwait
+  exec ${CONF_INSTALL}/bin/start-confluence.sh -fg
 else
   exec "$@"
 fi
