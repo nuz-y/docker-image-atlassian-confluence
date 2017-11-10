20f5a7384ad3cfda3d0633551c85785af78994ab
em <eugen.mayer@kontextwork.de>
2017-11-10 17:04:34 +0100

    add boilerplate configuration so people can write custom scripts to e.g. pre-deploy a confluence, add wait for it

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -184,7 +184,7 @@ fi
 /usr/local/bin/custom_scripts.sh
 
 if [ "$1" = 'confluence' ]; then
-  source /usr/bin/dockerwait
+  /usr/local/bin/wait-for-it -h ${CONFLUENCE_DB_HOST} -p ${CONFLUENCE_DB_PORT}
   exec ${CONF_INSTALL}/bin/start-confluence.sh -fg
 else
   exec "$@"
